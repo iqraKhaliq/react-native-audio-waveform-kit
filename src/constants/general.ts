c581c1cb8360806918d91354bf0e3ae933c8abef
iqraKhaliq <iqra.khaliq459@gmail.com>
2026-05-05 17:34:31 +0500

    feat: add general constants for audio waveform configuration

---
+++ src/constants/general.ts
@@ -0,0 +1,11 @@
+
+export const HEIGHT = 40;
+export const BAR_WIDTH_FIXED = 3;
+export const GAP_FIXED = 2;
+export const VISIBLE_BARS = 40;
+export const GAP = 2;
+export const TARGET = 40;
+export const BAR_WIDTH = 2;
+export const MAX_BARS = 80;
+export const NOISE_THRESHOLD = 0.03;
+export const MIN_HEIGHT = 2;
