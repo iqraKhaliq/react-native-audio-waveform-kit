5b87c26798d4c982ed549922e2c9dfdd161050f4
iqraKhaliq <iqra.khaliq459@gmail.com>
2026-05-05 17:35:37 +0500

    feat: update audio assets and add new icon files

---
+++ src/assets/index.ts
@@ -0,0 +1,6 @@
+export const icons = {
+    play: require('./icons/play.png'),
+    pause: require('./icons/pause.png'),
+    record: require('./icons/record.png'),
+    recording: require('./icons/recording.png'),
+}
