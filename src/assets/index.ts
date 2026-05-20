71ac7264d67b1c4d50ae51f0b0535ad291e7e637
iqraKhaliq <iqra.khaliq459@gmail.com>
2026-05-20 15:40:13 +0500

    refactor: standardize import/export formatting and clean up code structure

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,7 +1,7 @@
 export const icons = {
-    play: require('./icons/play.png'),
-    pause: require('./icons/pause.png'),
-    record: require('./icons/record.png'),
-    recording: require('./icons/recording.png'),
-    delete: require('./icons/delete.png'),
-}
+  play: require('./icons/play.png'),
+  pause: require('./icons/pause.png'),
+  record: require('./icons/record.png'),
+  recording: require('./icons/recording.png'),
+  delete: require('./icons/delete.png'),
+};
